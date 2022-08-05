c4af80ad4aa9a8e723d3324535a2fe0d80ac141d
Bang <bhcao.85@gmail.com>
2022-08-05 14:14:03 -0500

    added a home route

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+const request = require('request');
+
+const logger = require('morgan');
+app.use(logger("dev"));
+
+app.use(express.static('static'));
+app.set('view engine', 'ejs');
+
+app.use(express.static("public"));
+app.use(express.static("views/img"));
+
+require("dotenv").config();
+
+app.get("/", (req, res)=> {
+    res.render("home");
+});
+
+app.get("/home", (req, res) => {
+    res.redirect("/home.ejs")
+})
+
+app.listen(3000, ()=> {
+    console.log("Movie App is listening on port 3000");
+});
