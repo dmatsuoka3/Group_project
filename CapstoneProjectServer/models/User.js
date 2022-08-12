58155c638b03fa8f4b2cfe460f62deaccb220083
Bang <bhcao.85@gmail.com>
2022-08-12 14:10:06 -0500

    updating my branch

--- CapstoneProjectServer/models/User.js
+++ CapstoneProjectServer/models/User.js
@@ -5,14 +5,17 @@ const userSchema = new mongoose.Schema(
     {
         name: {
             type: String,
+            required: true
            
         },
         username: {
             type: String,
+            required: true
            
         },
         email: {
             type: String,
+            required: true
             
         },
         phone: {
