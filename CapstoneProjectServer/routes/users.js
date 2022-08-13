81be18e1dc666c2f470309de471c21253964b6b5
Bang <bhcao.85@gmail.com>
2022-08-13 10:10:22 -0500

    updating route

--- CapstoneProjectServer/routes/users.js
+++ CapstoneProjectServer/routes/users.js
@@ -1,12 +1,11 @@
 const router = require("express").Router();
 const express = require("express");
 const app = express();
+
+
+
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
-
-    //body parser
-    app.use(express.json());
-    app.use(express.urlencoded({extended:false}));
 
 
 // BLUEPRINTS
@@ -21,71 +20,68 @@ router.use(
 );
 router.use(passport.initialize());
 router.use(passport.session());
-passport.use(new LocalStrategy(UserModel.authenticate({usernameField: "email"},)));
+passport.use(new LocalStrategy(UserModel.authenticate()));
 passport.serializeUser(UserModel.serializeUser());
 passport.deserializeUser(UserModel.deserializeUser());
 
 const isLoggedIn = (req, res, next) => {
-  if (req.isAuthenticated( )) {
+  if (req.isAuthenticated()) {
     return next();
   }
   res.redirect("/login");
 };
 
-
-//route to the hompage same as the login page
 router.get("/", (req, res) => {
+  res.redirect("/home");
+});
+
+router.get("/home", (req, res) => {
   res.render("home.ejs");
 });
 
-//route to the login page
-router.get("/login", (req, res) => {
-  res.render("login.ejs")
-})
-
-router.post("/login", passport.authenticate("local",
-{
-  successRedirect: "/userspage",
-  failureRedirect: "/login",
-})
-); 
-
-//route to the user's page
 router.get("/userspage", isLoggedIn, (req, res) => {
   res.render("userspage.ejs");
 });
 
-
 router.get("/signup", (req, res) => {
   res.render("signup.ejs");
-}); 
+});
 
 router.post("/signup", (req, res) => {
-    const newUser = new UserModel({ 
-      username: req.body.username, 
-     /*  email: req.body.email,
-      name: req.body.name,
-      email: req.body.email,
-      phone: req.body.phone,
-      bio: req.body.bio,
-      gender: req.body.gender,
-      website: req.body.website */
-    });
-    UserModel.register(newUser, req.body.password, function (err, user) {
-      if (err) {
-        console.log(err);
-        return res.render("signup.ejs");
-      } else {
-        passport.authenticate("local")(req, res, function () {
-          res.redirect("/userspage");
-        });
-      }
-    })
+  const newUser = new UserModel({ 
+    username: req.body.username, 
+    email: req.body.email,
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+    bio: req.body.bio,
+    gender: req.body.gender,
+    website: req.body.website
   });
+  UserModel.register(newUser, req.body.password, function (err, user) {
+    if (err) {
+      console.log(err);
+      return res.render("signup.ejs");
+    } else {
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/userspage");
+      });
+    }
+  })
+});
 
+router.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
 
-
-
+router.post("/login",passport.authenticate("local", {
+      successRedirect: "/userspage",
+      failureRedirect: "/login",
+  }),
+  function (req, res) {
+    // We don't need anything in our callback function
+  }
+);
 
 router.get("/logout", (req, res, next) => {
   req.logout((err) => {
@@ -95,6 +91,8 @@ router.get("/logout", (req, res, next) => {
     res.redirect("/");
   });
 });
+
+
 
 
 module.exports = router;
