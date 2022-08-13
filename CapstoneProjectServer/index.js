fb1b861a0c3ab88df3510eac0b3f5a2e2f704c2f
Bang <bhcao.85@gmail.com>
2022-08-13 09:49:22 -0500

    updating route

--- CapstoneProjectServer/index.js
+++ CapstoneProjectServer/index.js
@@ -3,6 +3,7 @@ const express =require("express");
 const mongoose = require("mongoose");
 const session = require("express-session")
 const app = express();
+const port = process.env.PORT || 3000;
 const logger = require("morgan");
     app.use(logger("dev"));
     app.use(express.static("public"));
@@ -12,44 +13,18 @@ const logger = require("morgan");
     app.set("view engine", "ejs");
     app.use(express.static("uploads"));
 
-    //body parser
-    app.use(express.json());
-    app.use(express.urlencoded({extended:false}));
 
 
-//imports all the routes here
+
+//CONNECTION TO DATABASE
+const connectDB = require("./database/connection");
+connectDB();
+
+//IMPORT ALL THE ROUTE HERE
 const userRoute = require("./routes/users")
 
-
-//CONNECTION Mongoose
-const { DB, URI, DB_AUTHSOURCE, DB_PASSWORD, DB_USERNAME} = process.env;
-const url = `${URI}/${DB}`;
-
-let connectionObject = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    authSource: DB_AUTHSOURCE,
-    user: DB_USERNAME,
-    pass: DB_PASSWORD
-};
-
-mongoose
- .connect(url, connectionObject)
- .then(() => {console.log(`Connected to the ${DB} database`);})
- .catch((error) => console.log(`Issues connecting to the ${DB} database: ${error}`));
-
-
-
-
-
-
- //routes handler 
+//ROUTE HANDLER
 app.use("", userRoute);
 
 
-
-
-
-
-const port = process.env.PORT || 3000;
-    app.listen(port, () => console.log(`Bravo Team app is listening on http://localhost:${port}`))
+app.listen(port, () => console.log(`Bravo Team app is listening on http://localhost:${port}`))
--- CapstoneProjectServer/routes/users.js
+++ CapstoneProjectServer/routes/users.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 const express = require("express");
 const app = express();
-
-
-
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
+
+    //body parser
+    app.use(express.json());
+    app.use(express.urlencoded({extended:false}));
 
 
 // BLUEPRINTS
@@ -20,68 +21,71 @@ router.use(
 );
 router.use(passport.initialize());
 router.use(passport.session());
-passport.use(new LocalStrategy(UserModel.authenticate()));
+passport.use(new LocalStrategy(UserModel.authenticate({usernameField: "email"},)));
 passport.serializeUser(UserModel.serializeUser());
 passport.deserializeUser(UserModel.deserializeUser());
 
 const isLoggedIn = (req, res, next) => {
-  if (req.isAuthenticated()) {
+  if (req.isAuthenticated( )) {
     return next();
   }
   res.redirect("/login");
 };
 
-router.get("/", (req, res) => {
-  res.redirect("/home");
-});
 
-router.get("/home", (req, res) => {
+//route to the hompage same as the login page
+router.get("/", (req, res) => {
   res.render("home.ejs");
 });
 
+//route to the login page
+router.get("/login", (req, res) => {
+  res.render("login.ejs")
+})
+
+router.post("/login", passport.authenticate("local",
+{
+  successRedirect: "/userspage",
+  failureRedirect: "/login",
+})
+); 
+
+//route to the user's page
 router.get("/userspage", isLoggedIn, (req, res) => {
   res.render("userspage.ejs");
 });
 
+
 router.get("/signup", (req, res) => {
   res.render("signup.ejs");
-});
+}); 
 
 router.post("/signup", (req, res) => {
-  const newUser = new UserModel({ 
-    username: req.body.username, 
-    email: req.body.email,
-    name: req.body.name,
-    email: req.body.email,
-    phone: req.body.phone,
-    bio: req.body.bio,
-    gender: req.body.gender,
-    website: req.body.website
+    const newUser = new UserModel({ 
+      username: req.body.username, 
+     /*  email: req.body.email,
+      name: req.body.name,
+      email: req.body.email,
+      phone: req.body.phone,
+      bio: req.body.bio,
+      gender: req.body.gender,
+      website: req.body.website */
+    });
+    UserModel.register(newUser, req.body.password, function (err, user) {
+      if (err) {
+        console.log(err);
+        return res.render("signup.ejs");
+      } else {
+        passport.authenticate("local")(req, res, function () {
+          res.redirect("/userspage");
+        });
+      }
+    })
   });
-  UserModel.register(newUser, req.body.password, function (err, user) {
-    if (err) {
-      console.log(err);
-      return res.render("signup.ejs");
-    } else {
-      passport.authenticate("local")(req, res, function () {
-        res.redirect("/userspage");
-      });
-    }
-  })
-});
 
-router.get("/login", (req, res) => {
-  res.render("login.ejs");
-});
 
-router.post("/login",passport.authenticate("local", {
-      successRedirect: "/userspage",
-      failureRedirect: "/login",
-  }),
-  function (req, res) {
-    // We don't need anything in our callback function
-  }
-);
+
+
 
 router.get("/logout", (req, res, next) => {
   req.logout((err) => {
@@ -91,8 +95,6 @@ router.get("/logout", (req, res, next) => {
     res.redirect("/");
   });
 });
-
-
 
 
 module.exports = router;
