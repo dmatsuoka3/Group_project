8dffb65590f84b8c4f7e59394982844a5b790743
dmatsuoka3 <daigomat@gmail.com>
2022-08-17 17:55:29 -0500

    Changed image table name

--- CapstoneProjectServer/models/Post.js
+++ CapstoneProjectServer/models/Post.js
@@ -20,4 +20,4 @@ let imageSchema = new mongoose.Schema({
 
 imageSchema.plugin(mongoose_delete);
 
-module.exports = mongoose.model("imagesPost", imageSchema);
+module.exports = mongoose.model("imagesPosts", imageSchema);
